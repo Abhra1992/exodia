c75a4a7733a46066f0485895b6fb31aad168b433
Abhra Basak <abhra.1992@live.com>
2013-01-08 19:51:00 +0530

    Exodia Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Exodia::Application.config.secret_token = '9b91b446ac285e75ad20a06f2bf26d6a4497983257a5bd11a26f0864e62b4577329a2ff1fec758b17cbbc2bd055114de8f62ef05b0b377a268eaa4a3e6227c2b'
