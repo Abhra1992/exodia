27cbc09c07431d232a02cf68673832e6cabf77ad
Abhra Basak <abhra.1992@live.com>
2013-01-09 14:33:25 +0530

    Associated event with type and venue

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
   belongs_to :venue
-  belongs_to :type
+  belongs_to :type, :class_name => 'EventType'
   attr_accessible :code, :description, :end, :judging, :name, :rounds, :rules, :start, :submit_to, :team
 end
--- app/models/event_type.rb
+++ app/models/event_type.rb
@@ -1,3 +1,4 @@
 class EventType < ActiveRecord::Base
+  has_many :events, :foreign_key => :type_id
   attr_accessible :name
 end
