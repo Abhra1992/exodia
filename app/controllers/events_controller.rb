de424c9af30b5d1a3c614b8ef92a00aff9678283
Abhra Basak <abhra.1992@live.com>
2013-01-10 19:58:22 +0530

    Allowed creation of event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,20 +1,45 @@
 class EventsController < ApplicationController
+  def venues_and_types
+    @venues = Venue.all.map { |v| [v.name, v.id] }
+    @types = EventType.all.map { |t| [t.name, t.id] }
+  end  
+  
   def index
+    @events = Event.all
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def new
+    venues_and_types
   end
   
   def create
+    @event = Event.new(params[:event])
+    if @event.save
+      flash[:success] = "The event was successfully created"
+      redirect_to events_path
+    else
+      flash[:error] = "There was an error saving the event"
+      redirect_to new_events_path
+    end
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
   
   def update
+    @event = Event.find(params[:id])
+    if @event.update_attributes(params[:event])
+      flash[:success] = "The event changes were successfully saved"
+      redirect_to event_path(@event)
+    else
+      flash[:error] = "There was an error saving the changes"
+      redirect_to edit_event_path(@event)
+    end
   end
   
   def destroy
--- app/models/event.rb
+++ app/models/event.rb
@@ -7,6 +7,8 @@ class Event < ActiveRecord::Base
 	has_many :participants, :class_name => 'User', :through => :participations, :source => :user
 	
   attr_accessible :code, :description, :end, :judging, :name, :rounds, :rules, :start, :submit_to, :team
+  # Allow contacts to create event with type and venue
+  attr_accessible :type_id, :venue_id
   
   validates :name, :code, :presence => true, :uniqueness => true
 	validates :description, :start, :end, :submit_to, :presence => true
