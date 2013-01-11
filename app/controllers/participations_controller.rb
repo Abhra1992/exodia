127446dd8b2af04d919711b0d7495f7a34f3e3a7
Abhra Basak <abhra.1992@live.com>
2013-01-11 17:32:11 +0530

    Allowed registration and leaving a contest

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -10,7 +10,7 @@ class ParticipationsController < ApplicationController
 	end
 	
 	def create
-	  if current_user.participations.create(:event_id => params[:event_id])
+	  if current_user.events.push(Event.find(params[:contest_id]))
   	  flash[:success] = "Successfully registered for event."
 	  else
 	    flash[:error] = "Registration failed. Please try again."
@@ -19,7 +19,7 @@ class ParticipationsController < ApplicationController
 	end
 	
 	def destroy
-	  @participation = current_user.participations.find_by_event_id(params[:event_id])
+	  @participation = current_user.participations.find_by_event_id(params[:contest_id])
 	  if @participation.destroy
 	    flash[:success] = "Successfully cancelled registration for event."
 	  else
